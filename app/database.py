f8c12a5bcbb952521956e178f68579bbb5dca4c0
Rafaele Di Feo <rafaeledifeo@gmail.com>
2026-01-29 20:18:36 +0100

    Fix database.py to use DATABASE_URL

--- app/database.py
+++ app/database.py
@@ -1,14 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://postgres:123456@localhost:5432/empresa_db"
-
-print("CADENA REAL:", repr(DATABASE_URL))
+# Render ya tiene DATABASE_URL en el entorno
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
+
 def get_db():
     db = SessionLocal()
     try:
